08a94ae2c27cb7228c1d766f496b2fccc3b892c2
Nathan Randriantsoa <randriantsoanathan1@gmail.com>
2025-01-20 14:59:49 +0100

    creation du projet

---
+++ todo/crud.py
@@ -0,0 +1,34 @@
+collection = []
+_id = 0
+
+class collection:
+    
+    @staticmethod
+    def get():
+        return collection[:] # return a copy of the list
+    
+    @staticmethod
+    def post(task):
+        global _id
+        _id += 1
+        task['id'] = _id
+        collection.append(task)
+        return task
+    
+class item:
+    
+    @staticmethod
+    def get(id):
+        for task in collection:
+            if task['id'] == id:
+                return task
+        return None
+    
+    @staticmethod
+    def delete(id):
+        for i, t in enumerate(collection):
+            if t['id'] == id:
+                del collection[i]
+                return True
+        return False    
+    
